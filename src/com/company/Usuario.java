95d855994f23cb65945cf46cc1ff75ec44136868
Edux <edu,paniagua@pucp.edu.pe>
2020-09-15 08:23:58 -0500

    Clase Usuario

---
+++ src/com/company/Usuario.java
@@ -0,0 +1,62 @@
+package com.company;
+
+public class Usuario {
+    private Integer codigo;
+    private String nombres;
+    private String apellido;
+    private String telefono;
+    private String Anexo;
+
+    public Integer getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(Integer codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getAnexo() {
+        return Anexo;
+    }
+
+    public void setAnexo(String anexo) {
+        Anexo = anexo;
+    }
+
+    public String getDepartamento() {
+        return departamento;
+    }
+
+    public void setDepartamento(String departamento) {
+        this.departamento = departamento;
+    }
+
+    private String departamento;
+
+
+
+}
