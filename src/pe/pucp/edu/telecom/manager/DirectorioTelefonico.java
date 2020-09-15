2a1c4383564c454235508f446098ee7648444a0d
Samuel-Ayala <a20166262@pucp.edu.pe>
2020-09-15 09:53:52 -0500

    iml

--- src/pe/pucp/edu/telecom/manager/DirectorioTelefonico.java
+++ src/pe/pucp/edu/telecom/manager/DirectorioTelefonico.java
@@ -2,7 +2,9 @@ package pe.pucp.edu.telecom.manager;
 
 import com.company.Usuario;
 
+import java.io.FileWriter;
 import java.util.ArrayList;
+import java.util.List;
 
 
 public class DirectorioTelefonico {
@@ -18,8 +20,20 @@ public class DirectorioTelefonico {
         usu.setCodigo(usu.getCodigo());
         usu.setTelefono(usu.getTelefono());
         usu.setDepartamento(usu.getDepartamento());
-
         listaUsuario.add(usu);
+
+        List<String[]> paises = new ArrayList<String[]>;
+        paises.add(new String[]{"Afghanistan", "AF", "AFG", "4", "Yes"});
+        paises.add(new String[]{"Spain", "ES", "ESP", "724", "Yes"});
+
+
+        String archCSV = "/doc/Directorio.csv";
+        CSVWriter writer = new CSVWriter(new FileWriter(archCSV));
+
+        writer.writeAll(paises);
+
+        writer.close();
+
 
 
 
