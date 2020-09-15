b1e32225d491cd6d4245068acd04237c1c4e5dc7
Edux <edu,paniagua@pucp.edu.pe>
2020-09-15 08:34:50 -0500

    Clase Docente y JefeP

---
+++ src/com/company/JefePractica.java
@@ -0,0 +1,42 @@
+package com.company;
+
+import com.sun.org.apache.bcel.internal.generic.INEG;
+
+public class JefePractica extends Usuario{
+    private  Integer  egreso;
+    private  String profeDicta;
+    private  Integer instructorActual;
+    public Integer getEgreso() {
+        return egreso;
+    }
+
+    public void setEgreso(Integer egreso) {
+        this.egreso = egreso;
+    }
+
+    public String getProfeDicta() {
+        return profeDicta;
+    }
+
+    public void setProfeDicta(String profeDicta) {
+        this.profeDicta = profeDicta;
+    }
+
+    public Integer getInstructorActual() {
+        return instructorActual;
+    }
+
+    public void setInstructorActual(Integer instructorActual) {
+        this.instructorActual = instructorActual;
+    }
+
+
+
+
+
+
+
+
+
+
+}
