f2357672486ae6faf31ba9a2b779a3508a52e91c
Samuel-Ayala <a20166262@pucp.edu.pe>
2020-09-15 08:14:38 -0500

    primer commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,46 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc= new Scanner(System.in);
+        boolean end = false;
+        while(true)
+        {
+            String a=sc.nextLine();
+            System.out.println(a);
+            switch(a)
+            {
+                case "1":
+                {
+                    break;
+                }
+                case "2":
+                {
+                    break;
+                }
+                case "3":
+                {
+                    break;
+
+                }
+                case "4":
+                {
+                    break;
+                }
+                case "5":
+                {
+                    break;
+                }
+                default:
+                {
+                    end=true;
+                }
+            }
+
+        }
+    }
+}
