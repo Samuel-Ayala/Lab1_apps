22b0829bdcc9610c739f2ee8131f59ad61e781c8
Samuel-Ayala <a20166262@pucp.edu.pe>
2020-09-15 09:36:13 -0500

    asd

--- src/pe/pucp/edu/telecom/entity/LeerDatos.java
+++ src/pe/pucp/edu/telecom/entity/LeerDatos.java
@@ -1,13 +1,20 @@
 package pe.pucp.edu.telecom.entity;
 
-import java.io.DataInputStream;
-import java.io.FileInputStream;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvException;
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class LeerDatos {
 
+    String archCSV = "/doc/Directorio.csv";
+    CSVReader csvReader = new CSVReader(new FileReader(archCSV));
+    List<String[]> datos = csvReader.readAll();
+
+    public LeerDatos() throws IOException, CsvException {
+        System.out.println("Documento vacío");
+    }
 }
